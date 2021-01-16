947a8e933a2338556f13286bd1b4a5baccc0e919
Khalfani Wadlington <khalfani@khalfaniw.com>
2021-01-16 15:05:44 +0000

    src/components/time-reducer.js

--- src/components/time-reducer.js
+++ src/components/time-reducer.js
@@ -65,7 +65,7 @@ export function timeReducer(state, action) {
         });
         const realTimePassed = action.timeSinceEpochMS - state.startTime;
         const x = realTimePassed - state.millisecondsPassed;
-        const y = x > 0 ? x % state.millisecondsPerTick : 0;
+        const y = x > 0 ? Math.floor(x / state.millisecondsPerTick) : 0;
         console.log(
           "original diff",
           realTimePassed - newState.millisecondsPassed,
