80360b697da03a20fb68c1e9ac7c28777e6b3b07
Khalfani Wadlington <khalfani@khalfaniw.com>
2021-01-16 04:00:14 +0000

    src/components/time-reducer.js

--- src/components/time-reducer.js
+++ src/components/time-reducer.js
@@ -39,7 +39,7 @@ export function timeReducer(state, action) {
         const skippedTicks = getSkippedTicks({
           currentTime: action.timeSinceEpochMS,
           previousTime: draftState.timeSinceEpochMS,
-          interval: draftState.millisecondsPassed,
+          interval: draftState.millisecondsPerTick,
         });
 
         newState = produce(state, (draftState) => {
