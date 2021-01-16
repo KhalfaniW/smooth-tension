dc32c5b4e1a0cbebce71fc578b2c73b5cd31b8d4
Khalfani Wadlington <khalfani@khalfaniw.com>
2021-01-16 14:32:38 +0000

    src/components/time-reducer.js

--- src/components/time-reducer.js
+++ src/components/time-reducer.js
@@ -17,15 +17,12 @@ export function timeReducer(state, action) {
       case "HANDLE_UNRELIABLE_TIME_TICK":
         //setInterval ticks may be skipped some times
 
-        // if (skippedTicks1) {
-        //   newState = timeReducer(newState, {
-        //     type: "HANDLE_SKIPPED_TICKS",
-        //     timeSinceEpochMS: action.timeSinceEpochMS,
-        //   });
-        // }
-        times(skippedTicks1, () => {
-          newState = timeReducer(newState, {type: "HANDLE_TIME_TICK"});
-        });
+        if (skippedTicks1) {
+          newState = timeReducer(newState, {
+            type: "HANDLE_SKIPPED_TICKS",
+            timeSinceEpochMS: action.timeSinceEpochMS,
+          });
+        }
 
         if (
           action.timeSinceEpochMS - state.timeSinceEpochMS >=
