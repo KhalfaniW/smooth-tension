3120180e62d5456dcb7050fddcc3b888cbafc786
Khalfani Wadlington <khalfani@khalfaniw.com>
2021-01-16 13:23:37 +0000

    src/components/gamify.jsx

--- src/components/gamify.jsx
+++ src/components/gamify.jsx
@@ -18,7 +18,7 @@ export function Game({state = createState(), seed = Date.now()}) {
   const [gameState, setState] = useState({
     ...state,
     seed: seed,
-    millisecondsPerTick: 1,
+    millisecondsPerTick: 490,
 
     timeSinceEpochMS: Date.now(),
   });
